a82f2ae42f962243da00fc006a96b7c60fb318e8
mhdiya43 <mahdiyaelghan@gmail.com>
2025-12-26 13:56:02 +0100

    add code to class main

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        Calcul calc = new Calcul();
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+       
+    }
+}
+
